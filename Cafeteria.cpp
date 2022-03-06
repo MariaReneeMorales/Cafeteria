a1950ebcf06dd812f418672007547ec15e2d386b
María Reneé Morales Mijangos <99778960+MariaReneeMorales@users.noreply.github.com>
2022-03-05 19:33:13 -0600

    Agregando Librerías de C++

---
+++ Cafeteria.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+#include <iostream> 
+#include <math.h> 
+#include <string.h> 
+using namespace std; 
+
+int main() {
+	//Permite imprimir tildes, ñ o caracteres especiales. 
+	setlocale(LC_ALL, "");
+	
+	return 0;
+}
