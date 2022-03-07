c4178c1f28483071441835fc524e5963e6136da0
María Reneé Morales Mijangos <99778960+MariaReneeMorales@users.noreply.github.com>
2022-03-07 17:22:09 -0600

    Update Cafeteria.cpp

--- Cafeteria.cpp
+++ Cafeteria.cpp
@@ -2,10 +2,12 @@
 #include <iostream> 
 #include <math.h> 
 #include <string.h> 
-using namespace std; 
+#include <conio.h>
+#include <windows.h>
+using namespace std;
 
 int main() {
-	//Permite imprimir tildes, ñ o caracteres especiales. 
+	//Permite imprimir tildes, Ã± o caracteres especiales. 
 	setlocale(LC_ALL, "");
 	
 	return 0;
